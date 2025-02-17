cc658a2129bffcdaecde48c3cd5ebcba5a5e7210
mizanor21 <mizanorrahman398@gmail.com>
2025-02-17 15:48:28 +0600

    solution part polishing

--- components/Home/Solutions/Solutions.jsx
+++ components/Home/Solutions/Solutions.jsx
@@ -9,7 +9,7 @@ const Card = ({ section, index }) => {
     <div className="flex bg-white relative group font-sora overflow-hidden">
       <div
         key={index}
-        className={`hover-container ${section?.id} h-screen border-r-2 hover:text-white group`}
+        className={`hover-container h-screen border-r-2 hover:text-white group`}
       >
         <div className="text-center solutionCard w-[410px] min-[1000px]:w-[420px] min-[1400px]:w-[440px] 2xl:w-[580px] h-full flex items-center justify-center">
           <div className="px-5 lg:px-16">
@@ -18,13 +18,18 @@ const Card = ({ section, index }) => {
               <p className="text-[16px] font-[500] leading-[22px] text-gray-50 ">
                 {section?.content}
               </p>
-              <div className="flex justify-center pt-5 lg:pt-14">
+              <div className=" justify-center pt-5 lg:pt-14 hidden group-hover:flex">
                 <HiMiniArrowLongRight className="xl:w-20 arrowIcon xl:h-20 text-2xl lg:text-[40px] carousel-p text-[#0066B3] p-3  bg-white rounded-full group-hover:border-2 border-gray-300" />
               </div>
             </div>
           </div>
         </div>
       </div>
+      <style jsx>{`
+        .hover-container:hover {
+          background-color: ${section.color};
+        }
+      `}</style>
     </div>
   );
 };
